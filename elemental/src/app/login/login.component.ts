6afe5347d5518048a62afda20ef2953be4bae18f
SimoneCampisi0 <s.campisi@contrader.it>
2023-05-24 17:34:38 +0200

    add first component

---
+++ elemental/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log("ciao")
+  }
+}
