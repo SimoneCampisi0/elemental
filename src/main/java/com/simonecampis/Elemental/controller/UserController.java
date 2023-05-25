901a350e3c55f201302f0e8e4c5958c0975fc48c
SimoneCampisi0 <s.campisi@contrader.it>
2023-05-25 12:02:10 +0200

    implementare loginDTO

--- src/main/java/com/simonecampis/Elemental/controller/UserController.java
+++ src/main/java/com/simonecampis/Elemental/controller/UserController.java
@@ -10,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController extends AbstractController<UserDTO> {
 
+    @RequestMapping(value = "/login")
+    public UserDTO login () //realizzare LoginDTO
 }
