b2dbdbbad1df364fdcf040b6958effbb44a5255c
SimoneCampisi0 <s.campisi@contrader.it>
2023-05-25 15:08:07 +0200

    fix login

--- elemental/src/app/login/login-form/login-form.component.ts
+++ elemental/src/app/login/login-form/login-form.component.ts
@@ -3,8 +3,8 @@ import { FormGroup, FormControl } from '@angular/forms';
 import {UserService} from "../../../service/user.service";
 import {LoginDTO} from "../../../dto/logindto";
 import {UserDTO} from "../../../dto/userdto";
-
-@Component({
+import Swal from 'sweetalert2';
+ @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
@@ -41,6 +41,24 @@ export class LoginFormComponent {
         let user: UserDTO = new UserDTO(userGet.id, userGet.email, userGet.password)
         localStorage.setItem("currentUser", JSON.stringify(user))
       // }
+
     })
   }
+
+   terminiAlert() {
+     Swal.fire({
+       icon: 'info',
+       title: 'Termini e Condizioni',
+       text: 'Accedendo e utilizzando questa piattaforma, accetti di rispettare i seguenti Termini e Condizioni:\n\n1. Non violare i diritti di proprietà intellettuale altrui.\n2. Non diffondere contenuti offensivi, diffamatori o illegali.\n3. Rispettare la privacy degli altri utenti.\n4. Non utilizzare la piattaforma per scopi commerciali senza autorizzazione.\n5. Mantenere la sicurezza del tuo account e non condividere le tue credenziali di accesso.\n\nTi preghiamo di leggere attentamente i Termini e le Condizioni complete disponibili sul nostro sito web.',
+     });
+   }
+
+   policyAlert() {
+     Swal.fire({
+       icon: 'info',
+       title: 'Privacy Policy',
+       text: 'Privacy Policy.'
+     });
+   }
+
 }
