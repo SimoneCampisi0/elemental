8dcc1947d34f243e46cae11e675061d6e8ca2944
SimoneCampisi0 <s.campisi@contrader.it>
2023-05-30 16:58:33 +0200

    some fix in iterazione

--- elemental/src/app/home/content/elenco-post/post/post.component.ts
+++ elemental/src/app/home/content/elenco-post/post/post.component.ts
@@ -5,6 +5,9 @@ import {AnagService} from "../../../../../service/anag.service";
 import {Observable} from "rxjs";
 import {PostDTO} from "../../../../../dto/postdto";
 import {AnagDTO} from "../../../../../dto/anagdto";
+import {IterazioneService} from "../../../../../service/iterazione.service";
+import {UserDTO} from "../../../../../dto/userdto";
+import {IterazioneDTO} from "../../../../../dto/iterazionedto";
 
 @Component({
   selector: 'app-post',
@@ -20,13 +23,21 @@ export class PostComponent {
 
   // @ts-ignore
   anagAutore$: Observable<AnagDTO>;
+  // @ts-ignore
+  user: UserDTO;
 
+  // @ts-ignore
+  iterazione$: Observable<IterazioneDTO>
 
-  constructor(private anagService: AnagService, private postService: PostService) {
+  constructor(private anagService: AnagService, private itService: IterazioneService) {
   }
 
   ngOnInit() {
     this.anagAutore$ = this.anagService.findAnagByEmail(this.email)
+    // @ts-ignore
+    this.user = JSON.parse(localStorage.getItem('currentUser'))
+    this.iterazione$ = this.itService.findByIdUser(this.user.id)
+
 
   }
 
--- elemental/src/service/iterazione.service.ts
+++ elemental/src/service/iterazione.service.ts
@@ -2,6 +2,10 @@ import {Injectable} from "@angular/core";
 import {AbstractService} from "./abstractservice";
 import {HttpClient} from "@angular/common/http";
 import {IterazioneDTO} from "../dto/iterazionedto";
+import {LoginDTO} from "../dto/logindto";
+import {Observable} from "rxjs";
+import {UserDTO} from "../dto/userdto";
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +15,7 @@ export class IterazioneService extends AbstractService<IterazioneDTO>{
     super(http);
     this.type = 'iterazione';
   }
-
+  findByIdUser(id: number): Observable<IterazioneDTO> {
+    return this.http.get<IterazioneDTO>(environment.APIEndpoint + this.type + '/findByIdUser?id='+id)
+  }
 }
