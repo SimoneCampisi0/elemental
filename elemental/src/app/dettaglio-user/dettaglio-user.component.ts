8922159a87d55439ce34bb26c6de1448e0f71843
SimoneCampisi0 <s.campisi@contrader.it>
2023-06-14 12:59:58 +0200

    some fix in DettaglioUser

--- elemental/src/app/dettaglio-user/dettaglio-user.component.ts
+++ elemental/src/app/dettaglio-user/dettaglio-user.component.ts
@@ -73,5 +73,31 @@ export class DettaglioUserComponent {
     });
   }
 
+  modificaDati() {
+    Swal.fire({
+      title: 'Login Form',
+      html: `<input type="text" id="login" class="swal2-input" placeholder="Username">
+  <input type="password" id="password" class="swal2-input" placeholder="Password">`,
+      confirmButtonText: 'Sign in',
+      focusConfirm: false,
+      preConfirm: () => {
+        // @ts-ignore
+        const login = Swal.getPopup().querySelector('#login').value
+        // @ts-ignore
+        const password = Swal.getPopup().querySelector('#password').value
+        if (!login || !password) {
+          Swal.showValidationMessage(`Please enter login and password`)
+        }
+        return { login: login, password: password }
+      }
+    }).then((result) => {
+  //     Swal.fire(`
+  //   Login: ${result.value.login}
+  //   Password: ${result.value.password}
+  // `.trim())
+    })
+
+  }
+
 
 }
