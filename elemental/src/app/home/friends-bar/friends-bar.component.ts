63e63b3e50b651b602793764e2b540314720cd6a
SimoneCampisi0 <s.campisi@contrader.it>
2023-06-21 12:53:31 +0200

    fix somebugs friends-bar

--- elemental/src/app/home/friends-bar/friends-bar.component.ts
+++ elemental/src/app/home/friends-bar/friends-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {map, Observable, timer} from "rxjs";
+import { LogService } from "../../../service/logservice";
+import { switchMap, filter } from "rxjs/operators";
+import { UserDTO } from "../../../dto/userdto";
 
 @Component({
   selector: 'app-friends-bar',
@@ -6,6 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./friends-bar.component.css']
 })
 export class FriendsBarComponent {
+  //@ts-ignore
+  currentUser: UserDTO
+  //@ts-ignore
+  listaAmici$: Observable<UserDTO[]>
   amiciOnline: boolean = false
 
+  constructor(private logService: LogService) {}
+
+  ngOnInit() {
+    // @ts-ignore
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'))
+
+    this.listaAmici$ = timer(0, 2000).pipe(
+      switchMap(() => this.logService.getLoggedUsers()),
+      map((amici: UserDTO[]) => amici.filter(amico => amico.email !== this.currentUser.email))
+    );
+  }
 }
