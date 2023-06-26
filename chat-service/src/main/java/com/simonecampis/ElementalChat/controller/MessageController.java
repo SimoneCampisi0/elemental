166d302ee8e5c51d5d52c65ea462084ca4ac925e
SimoneCampisi0 <s.campisi@contrader.it>
2023-06-26 12:18:53 +0200

    fix gateway bug in Eureka

--- chat-service/src/main/java/com/simonecampis/ElementalChat/controller/MessageController.java
+++ chat-service/src/main/java/com/simonecampis/ElementalChat/controller/MessageController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/message")
+//@RequestMapping("/message")
 @CrossOrigin(origins = "http://localhost:4200")
 public class MessageController extends AbstractController<MessageDTO> {
     @Autowired
