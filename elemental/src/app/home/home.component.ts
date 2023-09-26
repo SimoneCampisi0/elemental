46e6281afe4a8c60e3dbfff6beb7c0376b8d7033
SimoneCampisi0 <s.campisi@contrader.it>
2023-09-26 15:53:17 +0200

    Fix CSS for ChatMenu

--- elemental/src/app/home/home.component.ts
+++ elemental/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ChatService } from 'src/service/chat.service';
 
 @Component({
@@ -7,10 +7,23 @@ import { ChatService } from 'src/service/chat.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public screenWidth: any
+  public screenHeight: any
+
   constructor (public chatService: ChatService) {}
 
   ngOnInit() {
     console.log("JWT: "+localStorage.getItem('token'))
     console.log("USER: "+localStorage.getItem('currentUser'))
   }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.screenWidth = window.innerWidth
+    this.screenHeight = window.innerHeight
+
+    console.log("this.screenWidth:",this.screenWidth);
+  }
+
+
 }
