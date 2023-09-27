97f43f2630f5b54400af8e796b8f6d3818b59554
SimoneCampisi0 <s.campisi@contrader.it>
2023-09-27 11:15:34 +0200

    Fix some TypeScript for BurgerMenu.

--- elemental/src/app/home/home.component.ts
+++ elemental/src/app/home/home.component.ts
@@ -12,12 +12,21 @@ export class HomeComponent {
   //@ts-ignore
   public screenUpResolution: boolean;
 
+  //@ts-ignore
+  public clickMenu: boolean;
+
   constructor (public chatService: ChatService) {}
 
   ngOnInit() {
     console.log("JWT: "+localStorage.getItem('token'))
     console.log("USER: "+localStorage.getItem('currentUser'))
     this.screenUpResolution = true;
+    this.clickMenu = false;
+
+    this.screenWidth = window.innerWidth
+    if(this.screenWidth < 1300) {
+      this.screenUpResolution = false;
+    }
   }
 
   @HostListener('window:resize', ['$event'])
@@ -25,10 +34,35 @@ export class HomeComponent {
     this.screenWidth = window.innerWidth
     this.screenHeight = window.innerHeight
 
-    if(this.screenWidth < 1300) {
+    if(this.screenWidth < 1300 && this.clickMenu == false) {
       this.screenUpResolution = false
-    } else {
+    }
+    if (this.screenWidth >= 1300){
       this.screenUpResolution = true
+    }
+  }
+
+  setClick() {
+    switch (this.clickMenu) {
+      case true:
+        this.clickMenu = false;
+        break;
+      case false:
+        this.clickMenu = true;
+        break;
+    }
+  }
+
+  openChatMenu() {
+    this.setClick()
+    switch (this.clickMenu) {
+      case true:
+        this.screenUpResolution = true
+        break;
+
+      case false:
+        this.screenUpResolution = false;
+        break;
     }
   }
 
